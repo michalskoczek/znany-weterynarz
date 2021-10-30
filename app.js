9d594082859e2e988718c12900e11f8d4726cd08
Michał Skoczek <mwskoczek@gmail.com>
2021-10-30 19:42:45 +0200

    Added CORS handling

--- app.js
+++ app.js
@@ -10,6 +10,16 @@ const app = express();
 app.use(express.urlencoded());
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, PATCH, DELETE',
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 app.use('/', homepageRoute);
 app.use('/vets', vetsRoute);
 app.use('/register', registerRoute);
