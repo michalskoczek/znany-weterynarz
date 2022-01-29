b977f64b1e454227f5216241f7417d1b956ba453
Michał Skoczek <mwskoczek@gmail.com>
2022-01-29 18:51:59 +0100

    Changed files extension js to ts

--- controllers/login.js
+++
@@ -1,3 +0,0 @@
-exports.getLoginPage = (req, res, next) => {
-  res.status(200).json({ title: 'Login page' });
-};
--- routes/homepage.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-
-const homepageController = require('../controllers/homepage');
-
-const router = Router();
-
-router.get('/', homepageController.getHomepage);
-
-module.exports = router;
